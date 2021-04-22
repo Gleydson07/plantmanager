19fc9f5032c13a9bccb253d9286f234dfe7d5b01
Gleydson07 <gassantos.dev@gmail.com>
2021-04-22 20:56:01 -0300

    first commit

---
+++ src/pages/Confirmation.tsx
@@ -0,0 +1,81 @@
+import React from 'react'
+import { 
+    SafeAreaView, 
+    View,
+    StyleSheet,
+    Text,
+} from 'react-native';
+
+import {Button} from '../components/Button';
+import colors from '../styles/colors';
+import fonts from '../styles/fonts';
+
+export function Confirmation(){
+    return(
+        <SafeAreaView style={styles.container}>
+            <View style={styles.content}>
+                <Text style={styles.emoji}>
+                    😁
+                </Text>
+
+                <Text style={styles.title}>
+                   Prontinho
+                </Text>
+
+                <Text style={styles.subtitle}>
+                    Agora vamos começar a cuidar das suas plantinhas com 
+                    muito cuidado.
+                </Text>
+                <View style={styles.footer}>
+                    <Button title="Começar"/>
+                </View>
+            </View>
+        </SafeAreaView>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'space-around',
+    },
+    
+    content: {
+        flex: 1,
+        width: '100%',
+        alignItems: 'center',
+        justifyContent: 'center',
+        padding: 30
+    },
+
+    emoji: {
+        fontSize: 72,
+        marginBottom: 20
+    },
+
+    title: {
+        fontSize: 22,
+        fontFamily: fonts.heading,
+        color: colors.heading,
+        textAlign: 'center',
+        lineHeight: 38,
+        marginTop: 15
+    },
+
+    subtitle: {
+        fontFamily: fonts.text,
+        textAlign: 'center',
+        fontSize: 17,
+        color: colors.heading,
+        paddingVertical: 10,
+        lineHeight: 38,
+    },
+
+    footer: {
+        width: '100%',
+        marginTop: 20,
+        paddingHorizontal: 50,
+
+    }
+})
